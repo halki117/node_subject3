2df4d108ac71c4e11ac0766ee888017dca777da1
halki117 <s.halki0203@icloud.com>
2023-05-02 00:37:51 +0900

    controller実装（クイズデータをjson返却）

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Get, Controller, Render, Res, Post } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from './app.service';
+import { QuizService } from './quiz/quiz.service';
+import { Quiz } from './quiz';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly quizService: QuizService) {}
 
   // @Get()
   // getHello(): string {
@@ -26,9 +28,19 @@ export class AppController {
   //   };
   // }
 
-  @Get('trivia')
-  async getTriviaData() {
-    return this.appService.getTriviaData();
+  @Get('quiz')
+  async getQuizData(): Promise<string> {
+    // クイズデータを取得、でーたはjson形式で返す
+    let quizData = await this.quizService.getQuizData();
+    return JSON.stringify(quizData);
   }
+
+
+  // @Get('trivia')
+  // async getTriviaData() {
+  //   return this.appService.getTriviaData();
+  // }
+
+
 
 }
