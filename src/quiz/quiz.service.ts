0402702efc8c1bcb06d19e9f553bbf488b706baa
halki117 <s.halki0203@icloud.com>
2023-05-01 10:44:04 +0900

    quiz.serviceの作成

---
+++ src/quiz/quiz.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { Quiz } from '../quiz';
+import axios from 'axios';
+
+@Injectable()
+export class QuizService {
+  private readonly URL = 'https://opentdb.com/api.php?amount=10';
+
+  async getQuizData(): Promise<Quiz[]>{
+    const resposnse = await axios.get(this.URL);
+    const quizData = resposnse.data.results;
+    return quizData.map((data: any) => new Quiz(data));
+  }
+}
